729b9727f2e7af69ec08db45f8f6f3b37049a9bd
Salkan81 <serkanalkant2f@gmail.com>
2022-03-22 16:43:18 +0300

    Dietician Form eklendi.

---
+++ DiyetProgramiProje/DieticianForm.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace DiyetProgramiProje
+{
+    partial class DieticianForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DieticianForm));
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.btnShowMeal = new System.Windows.Forms.Button();
+            this.cboxLvMealTime = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dtMealDate = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lvMeals = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.grpbox = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox7.SuspendLayout();
+            this.grpbox.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox7.Controls.Add(this.groupBox6);
+            this.groupBox7.Controls.Add(this.groupBox1);
+            this.groupBox7.Controls.Add(this.btnShowMeal);
+            this.groupBox7.Controls.Add(this.cboxLvMealTime);
+            this.groupBox7.Controls.Add(this.label10);
+            this.groupBox7.Controls.Add(this.dtMealDate);
+            this.groupBox7.Controls.Add(this.label8);
+            this.groupBox7.Controls.Add(this.lvMeals);
+            this.groupBox7.ForeColor = System.Drawing.SystemColors.Control;
+            this.groupBox7.Location = new System.Drawing.Point(596, 12);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(399, 599);
+            this.groupBox7.TabIndex = 15;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "Check Meals";
+            // 
+            // btnShowMeal
+            // 
+            this.btnShowMeal.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnShowMeal.Location = new System.Drawing.Point(276, 88);
+            this.btnShowMeal.Name = "btnShowMeal";
+            this.btnShowMeal.Size = new System.Drawing.Size(106, 35);
+            this.btnShowMeal.TabIndex = 19;
+            this.btnShowMeal.Text = "Show Meal";
+            this.btnShowMeal.UseVisualStyleBackColor = true;
+            // 
+            // cboxLvMealTime
+            // 
+            this.cboxLvMealTime.FormattingEnabled = true;
+            this.cboxLvMealTime.Location = new System.Drawing.Point(153, 96);
+            this.cboxLvMealTime.Name = "cboxLvMealTime";
+            this.cboxLvMealTime.Size = new System.Drawing.Size(106, 26);
+            this.cboxLvMealTime.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(11, 96);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 18);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Meal Time:";
+            // 
+            // dtMealDate
+            // 
+            this.dtMealDate.Location = new System.Drawing.Point(153, 39);
+            this.dtMealDate.Name = "dtMealDate";
+            this.dtMealDate.Size = new System.Drawing.Size(229, 24);
+            this.dtMealDate.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 45);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 18);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Meal Date:";
+            // 
+            // lvMeals
+            // 
+            this.lvMeals.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvMeals.GridLines = true;
+            this.lvMeals.HideSelection = false;
+            this.lvMeals.Location = new System.Drawing.Point(15, 137);
+            this.lvMeals.Name = "lvMeals";
+            this.lvMeals.Size = new System.Drawing.Size(367, 292);
+            this.lvMeals.TabIndex = 14;
+            this.lvMeals.UseCompatibleStateImageBehavior = false;
+            this.lvMeals.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Food";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Portion";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Calorie";
+            this.columnHeader4.Width = 90;
+            // 
+            // grpbox
+            // 
+            this.grpbox.BackColor = System.Drawing.Color.Transparent;
+            this.grpbox.Controls.Add(this.button1);
+            this.grpbox.Controls.Add(this.richTextBox1);
+            this.grpbox.Controls.Add(this.listView1);
+            this.grpbox.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.grpbox.Location = new System.Drawing.Point(13, 13);
+            this.grpbox.Name = "grpbox";
+            this.grpbox.Size = new System.Drawing.Size(577, 598);
+            this.grpbox.TabIndex = 16;
+            this.grpbox.TabStop = false;
+            this.grpbox.Text = "Clients";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(7, 23);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(564, 274);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(7, 304);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(564, 222);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(437, 532);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 27;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Name";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Surname";
+            this.columnHeader6.Width = 87;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Height";
+            this.columnHeader7.Width = 57;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Weight";
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Birthdate";
+            this.columnHeader9.Width = 81;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox6.Controls.Add(this.label7);
+            this.groupBox6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox6.Location = new System.Drawing.Point(216, 463);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(166, 64);
+            this.groupBox6.TabIndex = 21;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Daily Calorie Taken";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(56, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 24);
+            this.label7.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Location = new System.Drawing.Point(15, 462);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 65);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Daily Calorie Requirement";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(74, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 0;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "User Request";
+            // 
+            // DieticianForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1007, 623);
+            this.Controls.Add(this.grpbox);
+            this.Controls.Add(this.groupBox7);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DieticianForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DieticianForm";
+            this.Load += new System.EventHandler(this.DieticianForm_Load);
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
+            this.grpbox.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.Button btnShowMeal;
+        private System.Windows.Forms.ComboBox cboxLvMealTime;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DateTimePicker dtMealDate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListView lvMeals;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.GroupBox grpbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+    }
+}
